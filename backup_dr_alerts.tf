8cfb01348fb94c41ae7b65cf1aecfa3a24e199ac
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2025-06-05 18:14:13 +1000

    Update backup_dr_alerts.tf

--- backup_dr_alerts.tf
+++ backup_dr_alerts.tf
@@ -1,11 +1,11 @@
 # This file is for configuring alerts related to Google Cloud Backup and DR.
 
-provider "google" {
+#provider "google" {
   project = "glabco-sp-1"
   # Assuming the region might be needed for some monitoring resources,
   # let's add a common one. This can be adjusted if specific resources need otherwise.
   # region  = "us-west1"
-}
+#}
 
 # Resources for logging metric, alert policy, and notification channel will be added here.
 resource "google_monitoring_notification_channel" "backup_dr_failure_email_channel" {
