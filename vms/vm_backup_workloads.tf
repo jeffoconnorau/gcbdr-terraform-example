bbc6415e85122c40f6c99793d420b903ec2e4960
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2025-06-05 23:19:15 +1000

    Update vm_backup_workloads.tf

--- vms/vm_backup_workloads.tf
+++ vms/vm_backup_workloads.tf
@@ -22,40 +22,28 @@ data "google_compute_instance" "lax_linux_01" {
   zone     = "us-west2-c"       # Zone of the lax-linux-01 VM
 }
 
-resource "google_backup_dr_workload" "lax_linux_01_backup_workload" {
-  provider = google.gcp_bdr     # Use the provider for the Backup/DR project
-
-  project  = "glabco-bdr-1"     # Project where this workload config is stored
-  location = "us-west2"         # Location of the workload config & backup plan
-  name     = "lax-linux-01-workload" # Name for this workload configuration
-
-  type     = "COMPUTE_INSTANCE"
-  asset_name = data.google_compute_instance.lax_linux_01.self_link # Self-link of the VM
-
-  # Backup plan configuration
-  backup_plan = "projects/glabco-bdr-1/locations/us-west2/backupPlans/basic-vm-backup-plan-us-1"
-
-  # Other optional fields like 'description', 'labels' can be added if needed.
-}
-
 data "google_compute_instance" "lax_linux_02" {
   provider = google.gcp_compute # Use the provider for the project where the VM exists
   name     = "lax-linux-02"
   zone     = "us-west2-c"       # Zone of the lax-linux-02 VM
 }
 
-resource "google_backup_dr_workload" "lax_linux_02_backup_workload" {
-  provider = google.gcp_bdr     # Use the provider for the Backup/DR project
+resource "google_backup_dr_backup_plan_association" "lax_linux_01_plan_association" {
+  provider = google.gcp_bdr
+  project  = "glabco-bdr-1"
+  location = "us-west2"
+  name     = "lax-linux-01-basic-plan-assoc" # Name for the association resource
 
-  project  = "glabco-bdr-1"     # Project where this workload config is stored
-  location = "us-west2"         # Location of the workload config & backup plan
-  name     = "lax-linux-02-workload" # Name for this workload configuration
+  plan     = "projects/glabco-bdr-1/locations/us-west2/backupPlans/basic-vm-backup-plan-us-1"
+  resource = data.google_compute_instance.lax_linux_01.self_link # The resource to associate
+}
 
-  type     = "COMPUTE_INSTANCE"
-  asset_name = data.google_compute_instance.lax_linux_02.self_link # Self-link of the VM
+resource "google_backup_dr_backup_plan_association" "lax_linux_02_plan_association" {
+  provider = google.gcp_bdr
+  project  = "glabco-bdr-1"
+  location = "us-west2"
+  name     = "lax-linux-02-basic-plan-assoc" # Name for the association resource
 
-  # Backup plan configuration
-  backup_plan = "projects/glabco-bdr-1/locations/us-west2/backupPlans/basic-vm-backup-plan-us-1"
-
-  # Other optional fields like 'description', 'labels' can be added if needed.
+  plan     = "projects/glabco-bdr-1/locations/us-west2/backupPlans/basic-vm-backup-plan-us-1"
+  resource = data.google_compute_instance.lax_linux_02.self_link # The resource to associate
 }
