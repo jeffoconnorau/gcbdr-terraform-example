a57e71208ba6897b1f9c8b1d8d6b13f56fb16e59
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2025-06-05 23:27:57 +1000

    Update vm_backup_workloads.tf

--- vms/vm_backup_workloads.tf
+++ vms/vm_backup_workloads.tf
@@ -28,22 +28,35 @@ data "google_compute_instance" "lax_linux_02" {
   zone     = "us-west2-c"       # Zone of the lax-linux-02 VM
 }
 
+resource "google_backup_dr_backup_plan_association" "my-backup-plan-association" {
+  location = "us-central1"
+  resource_type= "compute.googleapis.com/Instance"
+  backup_plan_association_id          = "my-bpa"
+  resource      = google_compute_instance.myinstance.id
+  backup_plan  = google_backup_dr_backup_plan.bp1.name
+}
+
+
 resource "google_backup_dr_backup_plan_association" "lax_linux_01_plan_association" {
   provider = google.gcp_bdr
   project  = "glabco-bdr-1"
   location = "us-west2"
-  name     = "lax-linux-01-basic-plan-assoc" # Name for the association resource
-
-  plan     = "projects/glabco-bdr-1/locations/us-west2/backupPlans/basic-vm-backup-plan-us-1"
+  resource_type= "compute.googleapis.com/Instance"
+  #name     = "lax-linux-01-basic-plan-assoc" # Name for the association resource
+  backup_plan_association_id = "lax-linux-01-basic-plan-assoc"
   resource = data.google_compute_instance.lax_linux_01.self_link # The resource to associate
+  #plan     = "projects/glabco-bdr-1/locations/us-west2/backupPlans/basic-vm-backup-plan-us-1"
+  backup_plan  = google_backup_dr_backup_plan.basic-vm-backup-plan-us-1.name
 }
 
 resource "google_backup_dr_backup_plan_association" "lax_linux_02_plan_association" {
   provider = google.gcp_bdr
   project  = "glabco-bdr-1"
   location = "us-west2"
-  name     = "lax-linux-02-basic-plan-assoc" # Name for the association resource
-
-  plan     = "projects/glabco-bdr-1/locations/us-west2/backupPlans/basic-vm-backup-plan-us-1"
+  resource_type= "compute.googleapis.com/Instance"
+  #name     = "lax-linux-02-basic-plan-assoc" # Name for the association resource
+  backup_plan_association_id = "lax-linux-01-basic-plan-assoc"
   resource = data.google_compute_instance.lax_linux_02.self_link # The resource to associate
+  #plan     = "projects/glabco-bdr-1/locations/us-west2/backupPlans/basic-vm-backup-plan-us-1"
+  backup_plan_association_id = "lax-linux-02-basic-plan-assoc"
 }
