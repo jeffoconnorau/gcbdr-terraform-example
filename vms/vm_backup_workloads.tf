63944928ee6906ee97d5181a1a4ccf2d6724be66
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2025-06-05 23:53:29 +1000

    Update vm_backup_workloads.tf

--- vms/vm_backup_workloads.tf
+++ vms/vm_backup_workloads.tf
@@ -34,7 +34,7 @@ resource "google_backup_dr_backup_plan_association" "lax_linux_01_plan_associati
   location = "us-west2"
   #name     = "lax-linux-01-basic-plan-assoc" # Name for the association resource
   backup_plan_association_id          = "lax-linux-01-basic-plan-assoc"
-  plan     = google_backup_dr_backup_plan.us-vm-backup-plan-1.id
+  backup_plan     = google_backup_dr_backup_plan.us-vm-backup-plan-1.id
   resource = data.google_compute_instance.lax_linux_01.self_link # The resource to associate
 }
 
@@ -44,6 +44,6 @@ resource "google_backup_dr_backup_plan_association" "lax_linux_02_plan_associati
   location = "us-west2"
   #name     = "lax-linux-02-basic-plan-assoc" # Name for the association resource
   backup_plan_association_id          = "lax-linux-02-basic-plan-assoc"
-  plan     = google_backup_dr_backup_plan.us-vm-backup-plan-1.id
+  backup_plan     = google_backup_dr_backup_plan.us-vm-backup-plan-1.id
   resource = data.google_compute_instance.lax_linux_02.self_link # The resource to associate
 }
