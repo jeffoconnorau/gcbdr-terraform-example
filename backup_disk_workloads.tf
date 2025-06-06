e29f6455614aa3e308a4b4fc999c1d7b8f995a21
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2025-06-06 21:44:51 +1000

    Update backup_disk_workloads.tf

--- backup_disk_workloads.tf
+++ backup_disk_workloads.tf
@@ -21,22 +21,33 @@ data "google_compute_instance" "lax_linux_04" {
   zone     = "us-west2-c"       # Zone of the lax-linux-02 VM
 }
 
+#Example for Disk
+#resource "google_backup_dr_backup_plan_association" "bpa_1" {
+#  backup_plan_association_id = "bpa-1"
+#  project               = "vshreyansh-test-project-6"
+#  location              = "us-central1"
+#  backup_plan           = google_backup_dr_backup_plan.bp_disk_demo.id
+#  resource              = "//compute.googleapis.com/projects/vshreyansh-test-project-6/zones/us-central1-a/disks/test-disk"
+#  resource_type         = "compute.googleapis.com/Disk"
+#}
+
+
 resource "google_backup_dr_backup_plan_association" "lax_linux_03_plan_association" {
   provider = google.gcp_bdr
   project  = "glabco-sp-1"
   location = "us-west2"
-  resource = data.google_compute_instance.lax_linux_03.id # The resource to associate
-  backup_plan_association_id          = "lax-linux-03-basic-plan-assoc"
-  backup_plan = google_backup_dr_backup_plan.us-vm-backup-plan-1.id
-  resource_type= "compute.googleapis.com/Instance"
+  resource = "//compute.googleapis.com/projects/glabco-sp-1/zones/us-west2-c/disks/lax-linux-03" # The resource to associate
+  backup_plan_association_id          = "lax-linux-03-disk-plan-assoc"
+  backup_plan = google_backup_dr_backup_plan.us-disk-backup-plan-1.id
+  resource_type= "compute.googleapis.com/Disk" # for Regional Disk use /RegionDisk instead of /Disk
 }
 
 resource "google_backup_dr_backup_plan_association" "lax_linux_04_plan_association" {
   provider = google.gcp_bdr
   project  = "glabco-sp-1"
   location = "us-west2"
-  resource = data.google_compute_instance.lax_linux_04.id # The resource to associate
-  backup_plan_association_id          = "lax-linux-04-basic-plan-assoc"
+  resource = "//compute.googleapis.com/projects/glabco-sp-1/zones/us-west2-c/disks/lax-linux-04" # The resource to associate
+  backup_plan_association_id          = "lax-linux-04-disk-plan-assoc"
   backup_plan = google_backup_dr_backup_plan.us-disk-backup-plan-1.id
-  resource_type= "compute.googleapis.com/Instance"
+  resource_type= "compute.googleapis.com/Disk" # for Regional Disk use /RegionDisk instead of /Disk
 }
