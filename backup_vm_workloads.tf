bdf4edafaf1f7644e40890e9db9980cdedd581f4
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2025-06-06 23:24:34 +1000

    Update backup_vm_workloads.tf

--- backup_vm_workloads.tf
+++ backup_vm_workloads.tf
@@ -6,7 +6,7 @@ resource "google_backup_dr_backup_plan_association" "lax_linux_01_plan_associati
   provider = google.gcp_bdr
   project  = "glabco-sp-1"
   location = "us-west2"
-  resource = google_compute_instance.lax-linux-01.self_link # The resource to associate
+  resource = google_compute_instance.lax-linux-01.id # The resource to associate
   backup_plan_association_id          = "lax-linux-01-basic-plan-assoc"
   backup_plan = google_backup_dr_backup_plan.us-vm-backup-plan-1.id
   resource_type= "compute.googleapis.com/Instance"
@@ -19,7 +19,7 @@ resource "google_backup_dr_backup_plan_association" "lax_linux_02_plan_associati
   provider = google.gcp_bdr
   project  = "glabco-sp-1"
   location = "us-west2"
-  resource = google_compute_instance.lax-linux-02.self_link # The resource to associate
+  resource = google_compute_instance.lax-linux-02.id # The resource to associate
   backup_plan_association_id          = "lax-linux-02-basic-plan-assoc"
   backup_plan = google_backup_dr_backup_plan.us-vm-backup-plan-1.id
   resource_type= "compute.googleapis.com/Instance"
