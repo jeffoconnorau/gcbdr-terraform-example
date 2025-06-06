bdfef9e10e8bdcbbc9be66c05f6c676bad7b7c21
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2025-06-07 00:25:25 +1000

    Update backup_disk_workloads.tf

--- backup_disk_workloads.tf
+++ backup_disk_workloads.tf
@@ -6,9 +6,10 @@ resource "google_backup_dr_backup_plan_association" "lax_linux_03_plan_associati
   provider = google.gcp_bdr
   project  = "glabco-sp-1"
   location = "us-west2"
+  google_compute_instance.lax-linux-03.boot_disk[0].source
   #resource = google_compute_disk.lax-linux-03.boot_disk[0].device_name
   #resource = google_compute_instance.lax-linux-03.name
-  resource = data.google_compute_disk.lax-linux-03.id
+  #resource = data.google_compute_disk.lax-linux-03.id
   backup_plan_association_id          = "lax-linux-03-disk-plan-assoc"
   backup_plan = google_backup_dr_backup_plan.us-disk-backup-plan-1.id
   resource_type= "compute.googleapis.com/Disk" # for Regional Disk use /RegionDisk instead of /Disk
