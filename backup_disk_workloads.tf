73c005d7d170d2c60b0621e2321b9ea58a605f2c
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2025-06-06 23:41:05 +1000

    Update backup_disk_workloads.tf

--- backup_disk_workloads.tf
+++ backup_disk_workloads.tf
@@ -2,19 +2,19 @@
 # with backup plans.
 
 # Backup and DR Workload definitions will be added here.
-resource "google_backup_dr_backup_plan_association" "lax_linux_03_plan_association" {
-  provider = google.gcp_bdr
-  project  = "glabco-sp-1"
-  location = "us-west2"
-  resource = google_compute_instance.lax-linux-03.boot_disk[0].source
-  #resource = google_compute_instance.lax-linux-03.id
-  backup_plan_association_id          = "lax-linux-03-disk-plan-assoc"
-  backup_plan = google_backup_dr_backup_plan.us-disk-backup-plan-1.id
-  resource_type= "compute.googleapis.com/Disk" # for Regional Disk use /RegionDisk instead of /Disk
-  depends_on = [
-    google_compute_instance.lax-linux-03
-  ]
-}
+#resource "google_backup_dr_backup_plan_association" "lax_linux_03_plan_association" {
+#  provider = google.gcp_bdr
+#  project  = "glabco-sp-1"
+#  location = "us-west2"
+#  resource = google_compute_instance.lax-linux-03.boot_disk[0].source
+#  #resource = google_compute_instance.lax-linux-03.id
+#  backup_plan_association_id          = "lax-linux-03-disk-plan-assoc"
+#  backup_plan = google_backup_dr_backup_plan.us-disk-backup-plan-1.id
+#  resource_type= "compute.googleapis.com/Disk" # for Regional Disk use /RegionDisk instead of /Disk
+#  depends_on = [
+#    google_compute_instance.lax-linux-03
+#  ]
+#}
 
 resource "google_backup_dr_backup_plan_association" "lax_linux_03-d1_plan_association" {
   provider = google.gcp_bdr
@@ -29,19 +29,18 @@ resource "google_backup_dr_backup_plan_association" "lax_linux_03-d1_plan_associ
   ]
 }
 
-resource "google_backup_dr_backup_plan_association" "lax_linux_04_plan_association" {
-  provider = google.gcp_bdr
-  project  = "glabco-sp-1"
-  location = "us-west2"
-  resource = google_compute_instance.lax-linux-04.boot_disk[0].source
-  #resource = google_compute_instance.lax-linux-04.id
-  backup_plan_association_id          = "lax-linux-04-disk-plan-assoc"
-  backup_plan = google_backup_dr_backup_plan.us-disk-backup-plan-1.id
-  resource_type= "compute.googleapis.com/Disk" # for Regional Disk use /RegionDisk instead of /Disk
-  depends_on = [
-    google_compute_instance.lax-linux-04
-  ]
-}
+#resource "google_backup_dr_backup_plan_association" "lax_linux_04_plan_association" {
+#  provider = google.gcp_bdr
+#  project  = "glabco-sp-1"
+#  location = "us-west2"
+#  resource = google_compute_instance.lax-linux-04.boot_disk[0].source
+#  #resource = google_compute_instance.lax-linux-04.id
+#  backup_plan_association_id          = "lax-linux-04-disk-plan-assoc"
+#  resource_type= "compute.googleapis.com/Disk" # for Regional Disk use /RegionDisk instead of /Disk
+#  depends_on = [
+#    google_compute_instance.lax-linux-04
+#  ]
+# }
 
 resource "google_backup_dr_backup_plan_association" "lax_linux_04-d1_plan_association" {
   provider = google.gcp_bdr
