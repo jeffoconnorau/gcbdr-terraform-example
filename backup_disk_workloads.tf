24a3ac42a36bbc87e67aa048a2ddb7012fdb2078
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2025-06-07 00:45:07 +1000

    Update backup_disk_workloads.tf

--- backup_disk_workloads.tf
+++ backup_disk_workloads.tf
@@ -1,13 +1,13 @@
 # This file defines Backup and DR workload configurations to associate Disks
 # with backup plans.
 
-provider "google" {
+provider "google" { # this is being added to allow for direct use of the boot disks as google_compute_disk rather than google_compute
   alias   = "gcp_disk"
   project = "glabco-sp-1"
   region  = "us-west2" # VMs are in us-west2, so setting region for this provider
 }
 
-data "google_compute_disk" "lax_linux_03" {
+data "google_compute_disk" "lax_linux_03" { # this is being added to allow for direct use of the boot disks as google_compute_disk rather than google_compute
   provider = google.gcp_disk # Use the provider for the project where the Disk exists
   name     = "lax-linux-03"
   zone     = "us-west2-c"       # Zone of the lax-linux-01 VM
@@ -24,9 +24,12 @@ resource "google_backup_dr_backup_plan_association" "lax_linux_03_plan_associati
   provider = google.gcp_bdr
   project  = "glabco-sp-1"
   location = "us-west2"
-  resource = data.google_compute_disk.lax_linux_03.id
+  #below is working  
+  #resource = data.google_compute_disk.lax_linux_03.id
+  #below is not
+  google_compute_instance.lax-linux-03.boot_disk[0].source
   #resource = google_compute_disk.lax-linux-03.boot_disk[0].device_name
-  #resource = google_compute_instance.lax-linux-03.name
+  #resource = google_compute_instance.lax-linux-03.boot_disk[0].id
   #resource = data.google_compute_disk.lax-linux-03.id
   backup_plan_association_id          = "lax-linux-03-disk-plan-assoc"
   backup_plan = google_backup_dr_backup_plan.us-disk-backup-plan-1.id
