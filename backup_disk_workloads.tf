72e7e7038d8000f52fb051e17419870928356580
Jeff OConnor <35244024+jeffoconnorau@users.noreply.github.com>
2025-06-23 11:27:21 +1000

    Update backup_disk_workloads.tf

--- backup_disk_workloads.tf
+++ backup_disk_workloads.tf
@@ -1,33 +1,15 @@
 # This file defines Backup and DR workload configurations to associate Disks
 # with backup plans.
 
-provider "google" { # this is being added to allow for direct use of the boot disks as google_compute_disk rather than google_compute
-  alias   = "gcp_disk"
-  project = "glabco-sp-1"
-  region  = "us-west2" # VMs are in us-west2, so setting region for this provider
-}
-
-#data "google_compute_disk" "lax_linux_03" { # this is being added to allow for direct use of the boot disks as google_compute_disk rather than google_compute
-#  provider = google.gcp_disk # Use the provider for the project where the Disk exists
-#  name     = "lax-linux-03"
-#  zone     = "us-west2-c"       # Zone of the lax-linux-01 VM
-#  depends_on = [
-#    google_compute_instance.lax-linux-03
-#  ]
-#}
-
-#data "google_compute_disk" "lax_linux_04" {
-#  provider = google.gcp_disk # Use the provider for the project where the Disk exists
-#  name     = "lax-linux-04"
-#  zone     = "us-west2-c"       # Zone of the lax-linux-01 VM
-#  depends_on = [
-#    google_compute_instance.lax-linux-04
-#  ]
+#provider "google" { # this is being added to allow for direct use of the boot disks as google_compute_disk rather than google_compute
+#  alias   = "gcp_disk"
+#  project = "glabco-sp-1"
+#  region  = "us-west2" # VMs are in us-west2, so setting region for this provider
 #}
 
 # Backup and DR Workload definitions will be added here.
 resource "google_backup_dr_backup_plan_association" "lax_linux_03_plan_association" {
-  provider = google.gcp_bdr
+#  provider = google.gcp_bdr
   project  = "glabco-sp-1"
   location = "us-west2"
   resource = google_compute_instance.lax-linux-03.boot_disk[0].source
@@ -40,7 +22,7 @@ resource "google_backup_dr_backup_plan_association" "lax_linux_03_plan_associati
 }
 
 resource "google_backup_dr_backup_plan_association" "lax_linux_03-d1_plan_association" {
-  provider = google.gcp_bdr
+#  provider = google.gcp_bdr
   project  = "glabco-sp-1"
   location = "us-west2"
   resource = google_compute_disk.lax_linux_03_disk_1.id
@@ -53,10 +35,9 @@ resource "google_backup_dr_backup_plan_association" "lax_linux_03-d1_plan_associ
 }
 
 resource "google_backup_dr_backup_plan_association" "lax_linux_04_plan_association" {
-  provider = google.gcp_bdr
+#  provider = google.gcp_bdr
   project  = "glabco-sp-1"
   location = "us-west2"
-#  resource = data.google_compute_disk.lax_linux_04.id
   resource = google_compute_instance.lax-linux-04.boot_disk[0].source
   backup_plan_association_id          = "lax-linux-04-disk-plan-assoc"
   backup_plan = google_backup_dr_backup_plan.us-disk-backup-plan-1.id
@@ -67,7 +48,7 @@ resource "google_backup_dr_backup_plan_association" "lax_linux_04_plan_associati
  }
 
 resource "google_backup_dr_backup_plan_association" "lax_linux_04-d1_plan_association" {
-  provider = google.gcp_bdr
+#  provider = google.gcp_bdr
   project  = "glabco-sp-1"
   location = "us-west2"
   resource = google_compute_disk.lax_linux_04_disk_1.id
